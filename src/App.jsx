a59f3c4e830c7587e2b3862adde6dc1315718bfe
farhan.ali <farhan.ali@free-now.com>
2023-09-27 22:38:03 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { Canvas, useFrame } from "@react-three/fiber";
+import "./App.css";
+import { useRef } from "react";
+
+const Cube = ({ position, side, color }) => {
+  const ref = useRef();
+
+  useFrame((state, delta, frame) => {
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += delta;
+    ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2;
+  });
+
+  return (
+    <mesh position={position} ref={ref}>
+      <boxGeometry args={[side, side, side]} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
+
+const App = () => {
+  return (
+    <Canvas>
+      <directionalLight position={[0, 0, 1]} intensity={0.4} />
+      <ambientLight intensity={0.5} />
+      <group position={[0, -1, 0]}>
+        <Cube position={[1, 0, 0]} color={"green"} args={1} />
+        <Cube position={[-1, 0, 0]} color={"hotpink"} args={1} />
+        <Cube position={[-1, 2, 0]} color={"blue"} args={1} />
+        <Cube position={[1, 2, 0]} color={"yellow"} args={1} />
+      </group>
+    </Canvas>
+  );
+};
+
+export default App;
