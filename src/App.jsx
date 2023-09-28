b66d38ce84b5f785a203bc1a9297f1abbafc6deb
farhan.ali <farhan.ali@free-now.com>
2023-09-28 11:22:56 +0200

    drei and orbit controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import "./App.css";
 import { useRef, useState } from "react";
+import { OrbitControls } from "@react-three/drei";
 
 const Cube = ({ position, side, color }) => {
   const ref = useRef();
-
-  useFrame((state, delta, frame) => {
-    ref.current.rotation.x += delta;
-    ref.current.rotation.y += delta;
-    ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2;
-  });
 
   return (
     <mesh position={position} ref={ref}>
@@ -44,7 +39,7 @@ const TorusKnot = ({ position, args, color }) => {
   return (
     <mesh position={position} ref={ref}>
       <torusKnotGeometry args={args} />
-      <meshStandardMaterial color={color} wireframe />
+      <meshStandardMaterial color={color} />
     </mesh>
   );
 };
@@ -90,13 +85,12 @@ const App = () => {
         <Cube position={[-1, 2, 0]} color={"blue"} args={1} />
         <Cube position={[1, 2, 0]} color={"yellow"} args={1} />
       </group> */}
-      <Torus args={[1, 0.1, 50, 50]} color={"green"} position={[-3, 0, 0]} />
       <TorusKnot
         args={[1, 0.1, 1000, 50, 5]}
         position={[0, 0, 0]}
         color={"yellow"}
       />
-      <Sphere position={[3, 0, 0]} args={[1, 50, 50]} color={"orange"} />
+      <OrbitControls />
     </Canvas>
   );
 };
